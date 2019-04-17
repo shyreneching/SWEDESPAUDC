7c6849bbd3152bcc9673db57296a5e42599c8052
jaimepastor <41315228+jaimepastor@users.noreply.github.com>
2019-04-18 00:26:03 +0800

    fixed PlaylistUserCanFollower class to also include playlist of artist

--- SocialMusicPlayer/src/Model/PlaylistUserCanFollow.java
+++ SocialMusicPlayer/src/Model/PlaylistUserCanFollow.java
@@ -1,5 +1,6 @@
 package Model;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 import java.sql.SQLException;
@@ -7,14 +8,34 @@ import java.sql.SQLException;
 //NEW
 public class PlaylistUserCanFollow implements PlaylistList {
     private PlaylistService playlistService;
+    private AccountService accountService;
 
     public PlaylistUserCanFollow() {
         this.playlistService = new PlaylistService();
     }
 
+    //Collects public playlists as well as playlist of artists that they follow
     @Override
     public ObservableList<PlaylistInterface> createPlaylist(String username, ObservableList<SongInterface> s) throws SQLException{
-        ObservableList<PlaylistInterface> playlists = playlistService.getPublicPlaylist();
+        accountService = new AccountService();
+        ObservableList<Object> followedArtists = FXCollections.observableArrayList();
+        ObservableList<PlaylistInterface> playlists = FXCollections.observableArrayList();
+
+        //fills list of artists that user follows
+        for (Object user: accountService.getFollowed(username)){
+            if (user instanceof Artist){
+                followedArtists.add(user);
+            }
+        }
+
+        playlists.addAll(playlistService.getPublicPlaylist());//add all public playlists
+        String artistName;
+        //for each artist, add all the playlist that the artist has made; this is presuming every playlist of artist is open to the user
+        for (Object artist: followedArtists){
+            artistName = ((Artist)artist).getUsername();
+            playlists.addAll(playlistService.getUserPlaylist(artistName));
+        }
+
         return playlists;
     }
 }
